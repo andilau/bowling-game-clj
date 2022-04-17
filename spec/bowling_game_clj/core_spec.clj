3e912fa49e6812c45cddb5c10eed5da6ca085c06
Andi Lau <andi@herrlau.de>
2022-04-17 21:47:27 +0200

    Add strike & refactor

--- spec/bowling_game_clj/core_spec.clj
+++ spec/bowling_game_clj/core_spec.clj
@@ -1,15 +1,19 @@
 (ns bowling-game-clj.core-spec
-    (:require [speclj.core :refer :all]
-      [bowling-game-clj.core :refer :all]))
+    (:require [bowling-game-clj.core :refer :all]
+     [speclj.core :refer :all]))
 
 (describe "A bowling game"
           (describe "score"
                     (it "Gutter game results in a score of zero"
                         (should= 0 (score (repeat 0))))
                     (it "One pins game results in a score of twenty"
-                        (should= 20 (score (repeat 1)))))
-                    ;(it "Spare adds next roll to score"
-                    ;    (should= 16 (score [5 5 3])))
+                        (should= 20 (score (repeat 1))))
+                    (it "Spare adds next roll to score"
+                        (should= 16 (score [5 5 3])))
+                    (it "Strike adds next two rolls to score"
+                        (should= 28 (score [10 5 4])))
+                    (it "Perfect game results in a score of three-hundred"
+                        (should= 300 (score (repeat 10)))))
 
           (describe "-> frames"
                     (it "No rolls results in no frames"
@@ -21,7 +25,10 @@
                     (it "Three rolls results in one frame"
                         (should= [[1 2] [3]] (->frames [1 2 3])))
                     (it "Spare adds next row to frame"
-                        (should= [[5 5 4] [4]] (->frames [5 5 4])))))
+                        (should= [[5 5 4] [4]] (->frames [5 5 4])))
+                    (it "Strike adds next two row to frame"
+                        (should= [[10 5 4] [5 4]] (->frames [10 5 4])))))
+
 
 
 
--- src/bowling_game_clj/core.clj
+++ src/bowling_game_clj/core.clj
@@ -1,17 +1,29 @@
 (ns bowling-game-clj.core)
 
+(defn ten? [n] (= 10 n))
+
 (defn- sum [rolls] (reduce + rolls))
 
 (defn- spare? [rolls]
-       (= 10 (sum (take 2 rolls))))
+       (ten? (sum (take 2 rolls))))
+
+(defn- strike? [rolls]
+       (ten? (sum (take 1 rolls))))
+
+(defn rolls-for-frame [rolls]
+      (if (or (strike? rolls) (spare? rolls))
+       (take 3 rolls)
+       (take 2 rolls)))
+
+(defn rest-rolls [rolls] (if (strike? rolls)
+                          (drop 1 rolls)
+                          (drop 2 rolls)))
 
 (defn ->frames [rolls]
       (if (empty? rolls)
        []
-       (cons (if (spare? rolls)
-              (take 3 rolls)
-              (take 2 rolls))
-             (->frames (drop 2 rolls)))))
+       (cons (rolls-for-frame rolls)
+             (lazy-seq (->frames (rest-rolls rolls))))))
 
 (defn score [rolls]
-      (sum (take 20 rolls)))
+      (sum (flatten (take 10 (->frames rolls)))))
